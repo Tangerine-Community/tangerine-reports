a5ace2c6db7fd4f420da8f521a6688830aa2afae
R.J. Steinert <rj@rjsteinert.com>
2018-07-10 11:23:02 -0400

    Add better logging and longer sleep to start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,16 @@
 docker-compose down
 docker-compose build
 docker-compose up -d
-sleep 30
+
+echo ""
+echo ""
+echo ""
+echo "Sleeping for 60 seconds while services start..."
+sleep 60
 docker-compose exec -T elasticsearch bin/init_sg.sh
+
+echo ""
+echo ""
+echo ""
+echo "SYSTEM IS READY TO LOG INTO"
 
